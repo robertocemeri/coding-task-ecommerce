46b364ab58b798a4afb5b1575e9f4f7bd175d805
Roberto Cemeri <robertocemeri29@gmail.com>
2023-05-12 22:10:04 +0200

    Initial commit

---
+++ database/migrations/2023_05_11_204511_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('picture');
+            $table->double('start_price');
+            $table->double('buy_now_price');
+            $table->string('price_steps');
+            $table->dateTime('start_time')->default(null);
+            $table->dateTime('end_time')->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\Category::factory(10)->create();
+
+        \App\Models\Category::create([
+            'name' => 'Books',
+        ]);
+        \App\Models\Category::create([
+            'name' => 'Clothes',
+        ]);
+        \App\Models\Category::create([
+            'name' => 'Devices',
+        ]);
+        \App\Models\User::create([
+            'name' => 'Roberto Cemeri',
+            'email' => 'robertocemeri29@gmail.com',
+            'role' => 'admin',
+            'password' => Hash::make('roberto1'),
+        ]);
+    }
+}
